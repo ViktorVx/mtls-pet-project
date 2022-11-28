8063b9298bd067b44a25e04f86e1ceeeffc76a5f
ViktorVx <victorptrv@yandex.ru>
2022-11-28 20:34:23 +0300

    Init commit

---
+++ src/main/java/org/pva/mtlspetproject/MtlsPetProjectApplication.java
@@ -0,0 +1,13 @@
+package org.pva.mtlspetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtlsPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtlsPetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pva/mtlspetproject/MtlsPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pva.mtlspetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlsPetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
